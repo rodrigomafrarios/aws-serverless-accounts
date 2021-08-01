92cdd3fcc1320f35bb6f8cdec493e1a881afe054
Rodrigo Mafra <rodrigo.mafra@zenklub.com>
2021-08-01 17:57:07 -0300

    chore: add lint diff

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 module.exports = async () => {
   const { resources } = JSON.parse(fs.readFileSync('./tests/infra/db/dynamodb/migrations/account.json', 'utf8'))
   const tables = Object.keys(resources)
-  .map((name) => resources[name])
-  .filter((r) => r.Type === 'AWS::DynamoDB::Table')
-  .map((r) => r.Properties)
+    .map((name) => resources[name])
+    .filter((r) => r.Type === 'AWS::DynamoDB::Table')
+    .map((r) => r.Properties)
   return {
     port: 8000,
     tables
-  };
+  }
 }
